3f8901651d77be099cc66da75c3cb263d5ad07bd
renzo <renzolo1987@hotmail.com>
2024-01-12 12:25:31 +0100

    index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const morgan = require('./middlewares/morgan');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
 
